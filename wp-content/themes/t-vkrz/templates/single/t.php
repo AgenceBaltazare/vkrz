890ad756162241daff85911d18440dbaafbba4fe
gabriel <gabriel.pecheux@epitech.eu>
2020-05-21 22:58:41 +0200

    Add link to the Form

--- wp-content/themes/t-vkrz/templates/single/t.php
+++ wp-content/themes/t-vkrz/templates/single/t.php
@@ -2,9 +2,9 @@
 <?php
 $id_tournoi      = get_the_ID();
 $list_contenders = array();
-$all_user_votes       = new WP_Query( array(
+$all_user_votes       = new WP_Query(array(
     'post_type'      => 'vote',
-    'posts_per_page' => - 1,
+    'posts_per_page' => -1,
     'meta_query'     => array(
         'relation'   => 'AND',
         array(
@@ -20,47 +20,48 @@ $all_user_votes       = new WP_Query( array(
     )
 ));
 $nb_user_votes = $all_user_votes->post_count;
-$all_votes       = new WP_Query( array(
-	'post_type'      => 'vote',
-	'posts_per_page' => - 1,
-	'meta_query'     => array(
-		array(
-			'key'     => 'id_t_v',
-			'value'   => $id_tournoi,
-			'compare' => '=',
-		)
-	)
+$all_votes       = new WP_Query(array(
+    'post_type'      => 'vote',
+    'posts_per_page' => -1,
+    'meta_query'     => array(
+        array(
+            'key'     => 'id_t_v',
+            'value'   => $id_tournoi,
+            'compare' => '=',
+        )
+    )
 ));
-$contenders      = new WP_Query( array(
-	'post_type'      => 'contender',
-	'posts_per_page' => - 1,
-	'orderby'        => 'date',
-	'meta_query'     => array(
-		array(
-			'key'     => 'id_tournoi_c',
-			'value'   => $id_tournoi,
-			'compare' => '=',
-		)
-	)
+$contenders      = new WP_Query(array(
+    'post_type'      => 'contender',
+    'posts_per_page' => -1,
+    'orderby'        => 'date',
+    'meta_query'     => array(
+        array(
+            'key'     => 'id_tournoi_c',
+            'value'   => $id_tournoi,
+            'compare' => '=',
+        )
+    )
 ));
 $i               = 0;
-while ( $contenders->have_posts() ) : $contenders->the_post();
+while ($contenders->have_posts()) : $contenders->the_post();
 
-	array_push( $list_contenders, get_the_ID() );
+    array_push($list_contenders, get_the_ID());
 
-	$rand_c = array_rand( $list_contenders, 2 );
-	$id_c_1 = $list_contenders[ $rand_c[0] ];
-	$id_c_2 = $list_contenders[ $rand_c[1] ];
+    $rand_c = array_rand($list_contenders, 2);
+    $id_c_1 = $list_contenders[$rand_c[0]];
+    $id_c_2 = $list_contenders[$rand_c[1]];
 
-	$i ++; endwhile;
+    $i++;
+endwhile;
 
 $nums_pairs = "";
 $nb_battle  = 0;
-for ( $i = 0; $i <= count( $list_contenders ); $i ++ ) {
-	for ( $j = $i + 1; $j < count( $list_contenders ); $j ++ ) {
-		$nums_pairs .= $list_contenders[ $i ] . "," . $list_contenders[ $j ] . "<br>";
-		$nb_battle ++;
-	}
+for ($i = 0; $i <= count($list_contenders); $i++) {
+    for ($j = $i + 1; $j < count($list_contenders); $j++) {
+        $nums_pairs .= $list_contenders[$i] . "," . $list_contenders[$j] . "<br>";
+        $nb_battle++;
+    }
 }
 
 wp_reset_query();
@@ -71,20 +72,19 @@ wp_reset_query();
             <div class="row align-items-center">
                 <div class="col-6">
                     <div class="logo">
-                        <a href="<?php bloginfo( 'url' ); ?>/">
-                            <img src="<?php bloginfo( 'template_directory' ); ?>/assets/img/logo-vainkeurz-min.png"
-                                 alt="" class="img-fluid">
+                        <a href="<?php bloginfo('url'); ?>/">
+                            <img src="<?php bloginfo('template_directory'); ?>/assets/img/logo-vainkeurz-min.png" alt="" class="img-fluid">
                         </a>
                     </div>
                 </div>
                 <div class="col-6">
                     <div class="display_users_votes">
                         <h6>
-                            <?php if($nb_user_votes == 0): ?>
+                            <?php if ($nb_user_votes == 0) : ?>
                                 Aucun vote encore
-                            <?php elseif($nb_user_votes == 1): ?>
+                            <?php elseif ($nb_user_votes == 1) : ?>
                                 Bravo pour ton 1er vote
-                            <?php else: ?>
+                            <?php else : ?>
                                 Vos votes : <?php echo $all_user_votes->post_count; ?>
                             <?php endif; ?>
                         </h6>
@@ -100,10 +100,10 @@ wp_reset_query();
                     <div class="bloc-titre">
                         <h1 class="title-battle">
                             <b>
-								<?php the_title(); ?>
+                                <?php the_title(); ?>
                             </b>
                             <span>
-                                <?php the_field( 'question_t' ); ?>
+                                <?php the_field('question_t'); ?>
                             </span>
                         </h1>
                     </div>
@@ -117,7 +117,7 @@ wp_reset_query();
                 <div class="display_battle">
                     <div class="row align-items-center contenders-containers">
                         <div class="col-5 link-contender contender_1">
-	                        <?= formatContenderHtml($id_tournoi, $id_c_1, 1)?>
+                            <?= formatContenderHtml($id_tournoi, $id_c_1, 1) ?>
                         </div>
                         <div class="col-2">
                             <div class="display_votes">
@@ -130,18 +130,30 @@ wp_reset_query();
                             </h4>
                         </div>
                         <div class="col-5 link-contender contender_2">
-                            <?= formatContenderHtml($id_tournoi, $id_c_2, 2)?>
+                            <?= formatContenderHtml($id_tournoi, $id_c_2, 2) ?>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
+    <div>
+
+    </div>
     <div class="container-fluid">
         <div class="row">
             <div class="col-md-12">
                 <div class="classement_t row">
                     <?= getClassementHtml($id_tournoi) ?>
+                    <div class="form">
+                        <h6>
+                            <a href="https://surveyhero.com/c/b49c2b52">
+                                <div>
+                                    <button>Donnez nous votre avis !</button>
+                                </div>
+                            </a>
+                        </h6>
+                    </div>
                 </div>
             </div>
         </div>
