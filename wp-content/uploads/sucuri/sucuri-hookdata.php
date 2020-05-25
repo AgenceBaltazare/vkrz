b5e0e68ac1169e1f0b1722a62c035faf768cafcb
guillaumevergano <vainkeurz@bb0162.aquaray.com>
2020-05-25 13:44:47 +0200

    serveur

--- wp-content/uploads/sucuri/sucuri-hookdata.php
+++
@@ -1,6 +0,0 @@
-<?php
-// datastore=hookdata;
-// created_on=1589149411;
-// updated_on=1590328864;
-exit(0);
-?>
--- wp-content/uploads/sucuri/sucuri-integrity.php
+++
@@ -1,6 +0,0 @@
-<?php
-// datastore=integrity;
-// created_on=1588854670;
-// updated_on=1588854670;
-exit(0);
-?>
