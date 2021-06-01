6d872192ea948c4bd0af44eb54c9abb8e7cf833f
guillaumevergano <vergano.g@gmail.com>
2021-06-01 10:40:56 +0200

    Update menu-vkrz.php

--- wp-content/themes/t-vkrz-2/partials/menu-vkrz.php
+++ wp-content/themes/t-vkrz-2/partials/menu-vkrz.php
@@ -14,7 +14,7 @@
                     </div>
                 </a>
             </li>
-            <li class="nav-item nav-toggle">
+            <li class="nav-item nav-toggle hide-xs">
                 <a class="nav-link modern-nav-toggle pr-0" data-toggle="collapse">
                     <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-x d-block d-xl-none text-primary toggle-icon font-medium-4"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-disc d-none d-xl-block collapse-toggle-icon primary font-medium-4"><circle cx="12" cy="12" r="10"></circle><circle cx="12" cy="12" r="3"></circle>
                     </svg>
