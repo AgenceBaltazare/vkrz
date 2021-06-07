1429ab0cc39feaef5372e0ba53764554ab00c64f
guillaumevergano <vergano.g@gmail.com>
2021-06-07 02:55:30 +0200

    delete

---
+++ wp-content/themes/t-vkrz-3/templates/about.php
@@ -0,0 +1,34 @@
+<?php
+/*
+    Template Name: A propos
+*/
+?>
+<?php get_header(); ?>
+    <div class="app-content content ">
+        <div class="content-wrapper">
+            <div class="content-body">
+                <div class="blog-detail-wrapper">
+                    <div class="row">
+                        <div class="col-12">
+                            <div class="card">
+                                <div class="card-body apropos">
+                                    <h1>
+                                        Curieux d'en savoir plus <img src="<?php bloginfo('template_directory'); ?>/assets/images/vkrz/vainkeurz-text.png" alt="" class="img-fluid"> ?
+                                    </h1>
+                                    <h2 class="infoabout">
+                                        ⏲ <span class="space"></span> Temps de lecture estimé : <b>beaucoup trop</b>
+                                    </h2>
+                                    <div class="card-text mb-2 mt-2">
+                                        <?php while (have_posts()) : the_post(); ?>
+                                            <?php the_content(); ?>
+                                        <?php endwhile; ?>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+<?php get_footer(); ?>
