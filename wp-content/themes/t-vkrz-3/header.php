0caf71a21a5f91ec92a14f0f8c548b41e1296d4d
Vergano <vergano.g@gmail.com>
2021-06-17 08:56:51 +0200

    bug

--- wp-content/themes/t-vkrz-3/header.php
+++ wp-content/themes/t-vkrz-3/header.php
@@ -2,15 +2,15 @@
 global $uuiduser;
 global $user_id;
 $user_role = "visitor";
-if(is_user_logged_in()){
-    $current_user   = wp_get_current_user();
-    $user_id        = $current_user->ID;
-    $user_name      = $current_user->display_name;
-    $user_email     = $current_user->user_email;
-    $user_info      = get_userdata($user_id);
-    $user_role      = $user_info->roles[0];
-}
 if(get_post_type() != "tournoi" || !is_single()){
+    if(is_user_logged_in()){
+        $current_user   = wp_get_current_user();
+        $user_id        = $current_user->ID;
+        $user_name      = $current_user->display_name;
+        $user_email     = $current_user->user_email;
+        $user_info      = get_userdata($user_id);
+        $user_role      = $user_info->roles[0];
+    }
     $uuiduser           = deal_uuiduser();
 }
 ?>
--- wp-content/themes/t-vkrz-3/templates/single/t.php
+++ wp-content/themes/t-vkrz-3/templates/single/t.php
@@ -7,6 +7,14 @@ global $is_next_duel;
 global $id_tournament;
 global $id_ranking;
 global $is_next_duel;
+if(is_user_logged_in()){
+    $current_user   = wp_get_current_user();
+    $user_id        = $current_user->ID;
+    $user_name      = $current_user->display_name;
+    $user_email     = $current_user->user_email;
+    $user_info      = get_userdata($user_id);
+    $user_role      = $user_info->roles[0];
+}
 $uuiduser      = deal_uuiduser();
 $id_tournament = get_the_ID();
 $id_ranking    = get_or_create_ranking_if_not_exists($id_tournament, $uuiduser);
