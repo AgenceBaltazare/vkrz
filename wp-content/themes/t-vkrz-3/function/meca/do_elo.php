32fc252bca5ffc48b97efc9e0bc1a8f55b7618e2
Vergano <vergano.g@gmail.com>
2021-06-26 08:16:10 +0200

    clean

--- wp-content/themes/t-vkrz-3/function/meca/do_elo.php
+++ wp-content/themes/t-vkrz-3/function/meca/do_elo.php
@@ -11,19 +11,6 @@ function do_elo_ranking( $id_tournoi, $id_winner, $id_looser, $id_ranking ) {
 
 	update_field( 'ELO_c', round($newRankings['a']), $id_winner );
 	update_field( 'ELO_c', round($newRankings['b']), $id_looser );
-
-	$new_vote = array(
-		'post_type'   => 'vote',
-		'post_title'  => 'U:' . $uuiduser . ' T:' . $id_tournoi . ' V:' . $id_winner . ' L:' . $id_looser,
-		'post_status' => 'publish',
-	);
-	$id_vote  = wp_insert_post( $new_vote );
-
-	update_field( 'id_user_v', $uuiduser, $id_vote );
-	update_field( 'id_v_v', $id_winner, $id_vote );
-	update_field( 'id_l_v', $id_looser, $id_vote );
-	update_field( 'id_t_v', $id_tournoi, $id_vote );
-	update_field( 'id_r_v', $id_ranking, $id_vote );
 }
 
 
