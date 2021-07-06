feb61aabcd111286151eee2044091d32bf5bb383
Vergano <vergano.g@gmail.com>
2021-07-06 16:33:56 +0200

    Update account.php

--- wp-content/themes/t-vkrz-3/user/account.php
+++ wp-content/themes/t-vkrz-3/user/account.php
@@ -105,32 +105,6 @@ $list_t_created = $data_t_created[0]['creator_tops'];
                                     </div>
                                 </div>
                             </div>
-
-                            <!-- about -->
-                            <div class="card">
-                                <div class="card-body">
-                                    <h5 class="mb-75">About</h5>
-                                    <p class="card-text">
-                                        Tart I love sugar plum I love oat cake. Sweet ⭐️ roll caramels I love jujubes. Topping cake wafer.
-                                    </p>
-                                    <div class="mt-2">
-                                        <h5 class="mb-75">Joined:</h5>
-                                        <p class="card-text">November 15, 2015</p>
-                                    </div>
-                                    <div class="mt-2">
-                                        <h5 class="mb-75">Lives:</h5>
-                                        <p class="card-text">New York, USA</p>
-                                    </div>
-                                    <div class="mt-2">
-                                        <h5 class="mb-75">Email:</h5>
-                                        <p class="card-text">bucketful@fiendhead.org</p>
-                                    </div>
-                                    <div class="mt-2">
-                                        <h5 class="mb-50">Website:</h5>
-                                        <p class="card-text mb-0">www.pixinvent.com</p>
-                                    </div>
-                                </div>
-                            </div>
                         </div>
 
                         <div class="col-lg-9 col-12 order-1 order-lg-2">
