11be7ddd70a16ba5da0c34eaa4ca57aab9ccce4e
Vergano <vergano.g@gmail.com>
2021-07-07 23:02:05 +0200

    bar

--- wp-content/themes/t-vkrz-3/function/front.php
+++ wp-content/themes/t-vkrz-3/function/front.php
@@ -1,5 +1,5 @@
 <?php
     require_once('front/custom_login.php');
-    //require_once('front/move_adminbar.php');
+    require_once('front/move_adminbar.php');
     require_once('front/clean.php');
     require_once('front/scripts.php');
