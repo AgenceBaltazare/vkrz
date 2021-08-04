806c93440db76119395b534fde69cb383827f523
guillaumevergano <vergano.g@gmail.com>
2021-08-04 12:56:22 +0200

    Update header.php

--- wp-content/themes/t-vkrz-3/header.php
+++ wp-content/themes/t-vkrz-3/header.php
@@ -62,24 +62,8 @@ if(is_author()){
         <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
                     new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
                 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-5VVNJ52');</script>
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-KF4C954X96"></script>
-        <script>
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-KF4C954X96');
-        </script>
-        <script>
-            (function(h,o,t,j,a,r){
-                h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-                h._hjSettings={hjid:1825930,hjsv:6};
-                a=o.getElementsByTagName('head')[0];
-                r=o.createElement('script');r.async=1;
-                r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-                a.appendChild(r);
-            })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+                'https://www.googletagmanager.com/gtm.js?id=%27+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-KH379F5');
         </script>
     <?php endif; ?>
 
@@ -104,9 +88,11 @@ else{
 }
 ?>
 <body <?php body_class($list_body_class); ?> data-open="click" data-menu="vertical-menu-modern" data-col="">
-<noscript>
-    <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5VVNJ52" height="0" width="0" style="display:none;visibility:hidden"></iframe>
-</noscript>
+<?php if($user_role != "administrator"): ?>
+    <noscript>
+        <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KH379F5" height="0" width="0" style="display:none;visibility:hidden"></iframe>
+    </noscript>
+<?php endif; ?>
 
 <?php get_template_part('partials/menu-user'); ?>
 
