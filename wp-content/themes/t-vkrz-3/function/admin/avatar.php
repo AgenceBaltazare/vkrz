1015fa77b2335cc528ac2e28e8c96ba001569f9a
guillaumevergano <vergano.g@gmail.com>
2021-08-16 22:24:51 +0200

    css

--- wp-content/themes/t-vkrz-3/function/admin/avatar.php
+++ wp-content/themes/t-vkrz-3/function/admin/avatar.php
@@ -1,7 +1,7 @@
 <?php
 add_filter( 'avatar_defaults', 'new_gravatar' );
 function new_gravatar ($avatar_defaults) {
-    $myavatar = get_bloginfo('template_directory')."/assets/images/vkrz/avatar.png";
+    $myavatar = get_bloginfo('template_directory')."/assets/images/vkrz/avatar-rose.png";
     $avatar_defaults[$myavatar] = "VAINKEURZ Avatar";
     return $avatar_defaults;
 }
