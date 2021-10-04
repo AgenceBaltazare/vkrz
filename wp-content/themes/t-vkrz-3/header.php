ab17516e14c4ac4958b5ae15720ce53af5b2b224
Romain Rullier <71820670+RomainRullier@users.noreply.github.com>
2021-10-04 18:45:31 +0200

    creation t-marqueblanche.php

--- wp-content/themes/t-vkrz-3/header.php
+++ wp-content/themes/t-vkrz-3/header.php
@@ -129,6 +129,9 @@ if (is_single() || is_page(get_page_by_path('monitor'))) {
         <!-- End Google Tag Manager (noscript) -->
     <?php endif; ?>
 
+    <?php if(get_template_part('single/t-marqueblanche.php')): ?>
+        
+    <?php endif; ?>
     <?php
     get_template_part('partials/menu-user');
     get_template_part('partials/menu-vkrz');
