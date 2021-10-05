66563d756fb65a544b40e8e2eed1deda159d0909
Romain Rullier <71820670+RomainRullier@users.noreply.github.com>
2021-10-05 12:13:32 +0200

    update

--- wp-content/themes/t-vkrz-3/header.php
+++ wp-content/themes/t-vkrz-3/header.php
@@ -129,10 +129,14 @@ if (is_single() || is_page(get_page_by_path('monitor'))) {
         <!-- End Google Tag Manager (noscript) -->
     <?php endif; ?>
 
-    <?php if(get_template_part('single/t-marqueblanche.php')): ?>
-        
+    <?php if (!get_field('marqueblanche_t')): ?>
+        <?php
+        get_template_part('partials/menu-user');
+        get_template_part('partials/menu-vkrz');
+        ?>
+        <?php else: 
+             
+        ?>
     <?php endif; ?>
-    <?php
-    get_template_part('partials/menu-user');
-    get_template_part('partials/menu-vkrz');
-    ?>
+
+    
