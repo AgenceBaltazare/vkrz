b9614c940ddb2841136d69e0d368b293b98bd266
Vergano <vergano.g@gmail.com>
2021-10-05 09:26:00 +0200

    css

--- wp-content/themes/t-vkrz-3/templates/recrutement.php
+++ wp-content/themes/t-vkrz-3/templates/recrutement.php
@@ -11,7 +11,7 @@
                             <h1 class="text-uppercase">✊ Tu souhaites <span class="t-rose">créer des Tops</span> ?</h1>
                             <h3 class="mt-1">La 1<sup>ère</sup> étape pour rejoindre l'ékipe de créateurs 🎨 est de répondre à ceci 👇</h3>
                             <div class="typeform mt-3">
-                                <html> <head> <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"> <title>Recrutement</title> <style type="text/css">html { margin: 0; height: 100%; overflow: hidden; } iframe { position: absolute; top: 0; right: 0; bottom: 0; left: 0; border: 0; }</style> </head> <body> <iframe src="https://tally.so/embed/npG0qw?transparentBackground=1" width="100%" height="100%" frameborder="0" marginheight="0" marginwidth="0" title="Recrutement"></iframe> </body> </html>
+                                <iframe src="https://tally.so/embed/npG0qw?transparentBackground=1" width="100%" height="100%" frameborder="0" marginheight="0" marginwidth="0" title="Recrutement"></iframe>
                             </div>
                         </div>
                     </div>
