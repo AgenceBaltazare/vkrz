985aded707ddba55f63e384b9119727ad34de240
Vergano <vergano.g@gmail.com>
2021-10-23 11:38:24 +0200

    Update convert-top.php

--- wp-content/themes/t-vkrz-3/cron-job/convert-top.php
+++ wp-content/themes/t-vkrz-3/cron-job/convert-top.php
@@ -8,23 +8,24 @@ $all_tops = new WP_Query(array(
     'ignore_sticky_posts'    => true,
     'update_post_meta_cache' => false,
     'no_found_rows'          => true,
-    'post_type'              => 'tournoi',
+    'post_type'              => 'classement',
     'orderby'                => 'date',
     'order'                  => 'DESC',
     'posts_per_page'         => -1
 ));
 while ($all_tops->have_posts()) : $all_tops->the_post();
 
-    $id_top = get_the_ID();
+    $id_rank = get_the_ID();
+    $id_top = get_field('id_tournoi_r', );
     
     if(get_field('sponso_t', $id_top)){
-        wp_set_post_terms($id_top, 'sponso', 'type');
+        wp_set_post_terms($id_rank, 'sponso', 'type');
     } elseif (get_field('private_t', $id_top)) {
-        wp_set_post_terms($id_top, 'private', 'type');
+        wp_set_post_terms($id_rank, 'private', 'type');
     } elseif (get_field('marqueblanche_t', $id_top)) {
-        wp_set_post_terms($id_top, 'whitelabel', 'type');
+        wp_set_post_terms($id_rank, 'whitelabel', 'type');
     } else{
-        wp_set_post_terms($id_top, 'classik', 'type');
+        wp_set_post_terms($id_rank, 'classik', 'type');
     }
 
 endwhile; ?>
