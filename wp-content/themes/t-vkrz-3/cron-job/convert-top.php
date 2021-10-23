54dad5de240ec2f8d9420babacc156b3a01cedc9
Vergano <vergano.g@gmail.com>
2021-10-23 12:45:32 +0200

    Update convert-top.php

--- wp-content/themes/t-vkrz-3/cron-job/convert-top.php
+++ wp-content/themes/t-vkrz-3/cron-job/convert-top.php
@@ -26,9 +26,7 @@ while ($all_classement->have_posts()) : $all_classement->the_post();
     $id_rank = get_the_ID();
     $id_top  = get_field('id_tournoi_r', $id_rank);
 
-    echo $id_top."<br>";
-    echo get_field('sponso_t', $id_top) . "<br><br><br>";
-    if (get_field('sponso_t', $id_top)) {
+    if ($id_top == 192381) {
         wp_set_post_terms($id_rank, 'sponso', 'type');
     }
 
