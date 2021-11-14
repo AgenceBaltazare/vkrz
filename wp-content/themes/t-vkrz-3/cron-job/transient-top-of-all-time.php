7d394cc89ad51d8b0aef15e570ff7de863107cc7
Vergano <vergano.g@gmail.com>
2021-11-14 16:55:09 +0100

    Update transient-top-of-all-time.php

--- wp-content/themes/t-vkrz-3/cron-job/transient-top-of-all-time.php
+++ wp-content/themes/t-vkrz-3/cron-job/transient-top-of-all-time.php
@@ -10,7 +10,7 @@ include __DIR__.'/../../../../wp-load.php';
 $rankings = new WP_Query(
     array(
         'post_type'              => 'classement',
-        'posts_per_page'         => '40000',
+        'posts_per_page'         => '-1',
         'fields'                 => 'ids',
         'post_status'            => 'publish',
         'ignore_sticky_posts'    => true,
