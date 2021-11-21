16e8c606ac078403f1e0fdff76d6a2d37b33323b
Vergano <vergano.g@gmail.com>
2021-11-21 22:17:09 +0100

    Update packs.php

--- wp-content/themes/t-vkrz-3/function/API/packs.php
+++ wp-content/themes/t-vkrz-3/function/API/packs.php
@@ -1,23 +1,16 @@
 <?php
 require_once('fct.php');
 
-
 function get_single_ranking($data){
 
-    $datas      = array();
-    $id_ranking = $data['id_ranking'];
+    $id_ranking   = $data['id_ranking'];
     $user_ranking = get_user_ranking($id_ranking);
-    $total_rank  = array();
+    $total_rank   = "";
 
-    $i=1;
     foreach ($user_ranking as $c) :
 
-        array_push($total_rank, array(
-            "place"     => $i,
-            "contender" => get_the_title($c)
-        ));
-        
-    $i++;
+        $total_rank .= get_the_title($c)." ";
+    
     endforeach;
 
     return $total_rank;
