02958f7f60b48d893b75e3c306bb8cb29ca08b96
Vergano <vergano.g@gmail.com>
2021-11-21 22:21:40 +0100

    Update packs.php

--- wp-content/themes/t-vkrz-3/function/API/packs.php
+++ wp-content/themes/t-vkrz-3/function/API/packs.php
@@ -1,17 +1,25 @@
 <?php
 require_once('fct.php');
 
+
 function get_single_ranking($data){
 
-    $id_ranking   = $data['id_ranking'];
+    $list_ranking      = "";
+    $id_ranking = $data['id_ranking'];
     $user_ranking = get_user_ranking($id_ranking);
-    $total_rank   = "";
+    $total_rank  = array();
 
+    $i=1;
     foreach ($user_ranking as $c) :
 
-        $total_rank .= get_the_title($c)." ";
-    
+        $list_ranking .= get_the_title($c) . " ";
+
+    $i++;
     endforeach;
 
+    array_push($total_rank, array(
+        "classement" => $list_ranking,
+    ));
+        
     return $total_rank;
 }
