20a63decb32ee96ca9909777371ba759fa081a0e
Vergano <vergano.g@gmail.com>
2021-12-12 10:00:17 +0100

    Update count.php

--- wp-content/themes/t-vkrz-3/cron-job/count.php
+++ wp-content/themes/t-vkrz-3/cron-job/count.php
@@ -6,6 +6,8 @@ $nb_tops_complete   = 0;
 $nb_tops_again      = 0;
 $nb_tops_notdone    = 0;
 $nb_tops_triche     = 0;
+$type_top_3         = 0;
+$type_top_complet   = 0;
 
 $classement = new WP_Query(array(
     'ignore_sticky_posts'    => true,
@@ -18,16 +20,22 @@ $classement = new WP_Query(array(
 while ($classement->have_posts()) : $classement->the_post();
     
     if(get_field('done_r') == "done"){
-        $nb_tops_complete++;
+        $nb_tops_complete = $nb_tops_complete + 1;
     }
     else{
-        $nb_tops_notdone++;
+        $nb_tops_notdone = $nb_tops_notdone + 1;
     }
 
     $nb_votes = $nb_votes + get_field('nb_votes_r');
 
     if(get_field('suspected_cheating_r')){
         $nb_tops_triche++;
+    }
+
+    if (get_field('type_top_r') == "top3") {
+        $type_top_3++;
+    } elseif (get_field('type_top_r') == "complet") {
+        $type_top_complet++;
     }
 
     if(get_post_status() == 'draft'){
@@ -39,9 +47,14 @@ endwhile;
 $count_tops     = $classement->post_count;
 $pr_triche      = $nb_tops_triche * 100 / $count_tops;
 $pr_complete    = $nb_tops_complete * 100 / $count_tops;
+$pr_top3        = $type_top_3 * 100 / $count_tops;
+$pr_complet     = $type_top_complet * 100 / $count_tops;
 
 echo "Votes : ". $nb_votes."\n";
 echo "Tops : " . $classement->post_count . " (" . round($pr_complete) . "%)\n";
 echo "Tops finis: " . $nb_tops_complete."\n";
+echo "Tops non fini: " . $nb_tops_notdone . "\n";
 echo "Tops recommencés: " . $nb_tops_again . "\n";
-echo "Triche : " . $nb_tops_triche . " (". round($pr_triche)."%)\n";
+echo "Triche : " . $nb_tops_triche . " (" . round($pr_triche) . "%)\n";
+echo "Top 3 : " . $type_top_3 . " (" . round($pr_top3) . "%)\n";
+echo "Complet : " . $type_top_complet . " (". round($pr_complet)."%)\n";
