b88d2eeb9a3911b28e5d0a57b05a881f5d06b5f2
Vergano <vergano.g@gmail.com>
2021-12-16 15:05:57 +0100

    Create count-money.php

---
+++ wp-content/themes/t-vkrz-3/cron-job/count-money.php
@@ -0,0 +1,23 @@
+<?php
+include __DIR__ . '/../../../../wp-load.php';
+
+$total_money = 0;
+
+$vainkeur = new WP_Query(array(
+    "post_type"              => "vainkeur",
+    "posts_per_page"         => -1,
+    "fields"                 => "ids",
+    "post_status"            => "publish",
+    "orderby"                => "date",
+    "order"                  => "ASC",
+    "ignore_sticky_posts"    => true,
+    "update_post_meta_cache" => false,
+    "no_found_rows"          => false
+));
+while ($vainkeur->have_posts()) : $vainkeur->the_post();
+
+    $total_money = $total_money + get_field('money_vkrz');
+
+    echo $total_money;
+
+endwhile;
