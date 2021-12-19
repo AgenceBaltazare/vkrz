009dce45a89855544ee441108a1f9c7636d2f78d
Vergano <vergano.g@gmail.com>
2021-12-19 18:46:24 +0100

    Update do_transaction.php

--- wp-content/themes/t-vkrz-3/function/meca/do_transaction.php
+++ wp-content/themes/t-vkrz-3/function/meca/do_transaction.php
@@ -21,6 +21,7 @@ function do_transaction($id_produit, $user_uuid, $price, $user_email, $idvainkeu
 
 
         update_vainkeur_badge($idvainkeur, 'Shopper');
+        vkrz_push_transaction($id_new_transaction);
 
         return die(json_encode($current_money));
 
