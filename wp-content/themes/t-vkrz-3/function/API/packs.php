4e30d6672f45bb376413bfcb8609df0735dbcda1
Adil <adilelmoba20@gmail.com>
2022-04-12 11:50:53 +0200

    gsheets changement

--- wp-content/themes/t-vkrz-3/function/API/packs.php
+++ wp-content/themes/t-vkrz-3/function/API/packs.php
@@ -2,50 +2,119 @@
 require_once('fct.php');
 
 
-function get_single_ranking($data){
+function get_single_ranking($data)
+{
 
-    $list_ranking      = "";
-    $id_ranking        = $data['id_ranking'];
-    $user_ranking      = get_user_ranking($id_ranking);
-    $total_rank        = array();
+  $list_ranking      = "";
+  $id_ranking        = $data['id_ranking'];
+  $user_ranking      = get_user_ranking($id_ranking);
+  $total_rank        = array();
 
-    $i=1;
-    foreach ($user_ranking as $c) :
+  $i = 1;
+  foreach ($user_ranking as $c) :
 
-        $list_ranking .= get_the_title($c) . " ";
+    $list_ranking .= get_the_title($c) . " ";
 
     $i++;
-    endforeach;
+  endforeach;
 
-    array_push($total_rank, array(
-        "classement" => $list_ranking,
-    ));
-        
-    return $list_ranking;
+  array_push($total_rank, array(
+    "classement" => $list_ranking,
+  ));
+
+  return $list_ranking;
 }
 
-function add_contender_from_api(){
-    
-    $id_visual   = $_GET['idphoto'];
-    $url_visual  = $_GET['url_visual'];
-    $pseudo      = $_GET['pseudo'];
-    $id_top      = $_GET['id_top'];
+function add_contender_from_api()
+{
 
-    if ($id_visual) {
+  $id_visual   = $_GET['idphoto'];
+  $url_visual  = $_GET['url_visual'];
+  $pseudo      = $_GET['pseudo'];
+  $id_top      = $_GET['id_top'];
 
-        $new_contender = array(
-            'post_type'   => 'contender',
-            'post_title'  => $pseudo,
-            'post_status' => 'publish',
-        );
-        $id_new_contender  = wp_insert_post($new_contender);
+  if ($id_visual) {
 
-        update_field('visuel_instagram_contender', $url_visual, $id_new_contender);
-        update_field('id_tournoi_c', $id_top, $id_new_contender);
-        update_field('ELO_c', '1200', $id_new_contender);
+    $new_contender = array(
+      'post_type'   => 'contender',
+      'post_title'  => $pseudo,
+      'post_status' => 'publish',
+    );
+    $id_new_contender  = wp_insert_post($new_contender);
 
-        if($id_new_contender){
-            return "Nouveau contender dans le Top ".get_the_title($id_top);
-        }
+    update_field('visuel_instagram_contender', $url_visual, $id_new_contender);
+    update_field('id_tournoi_c', $id_top, $id_new_contender);
+    update_field('ELO_c', '1200', $id_new_contender);
+
+    if ($id_new_contender) {
+      return "Nouveau contender dans le Top " . get_the_title($id_top);
     }
+  }
+}
+
+function get_info()
+{
+  // TODAY'S DATE
+  $today = date('d-m-Y');
+  $todayFormatted = strtotime($today);
+  $yesterday = date("d-m-Y", strtotime("-1 day", $todayFormatted));
+
+  // COMPTE ENREGISTRE
+  $args = array(
+    'date_query' => array(
+      array(
+        'after'     => "$yesterday",
+        'before'    => "$today",
+        'inclusive' => true
+      )
+    )
+  );
+  $user_query = new WP_User_Query($args);
+  $comptes = $user_query->get_total();
+
+  // CLASSEMENT PUBLIE
+  $args2 = array(
+    'post_type' => 'classement',
+    'date_query' => array(
+      array(
+        'after'     => "$yesterday",
+        'before'    => "$today",
+        'inclusive' => true
+      )
+    )
+  );
+  $query2 = new WP_Query($args2);
+  $classements = $query2->found_posts;
+
+  // PLAYER PUBLIE
+  $args3 = array(
+    'post_type' => 'player',
+    'date_query' => array(
+      array(
+        'after'     => "$yesterday",
+        'before'    => "$today",
+        'inclusive' => true
+      )
+    )
+  );
+  $query3 = new WP_Query($args3);
+  $players = $query3->found_posts;
+
+  // CLASSEMENT NB VOTES
+  $nb_classement_votes = 0;
+  while ($query2->have_posts()) : $query2->the_post();
+    $nb_classement_votes =  $nb_classement_votes + get_field('nb_votes_r');
+  endwhile;
+
+  $results = array(
+    "KPI'S" => date("d-m-Y", strtotime($yesterday)),
+    "Compte enregistre" => $comptes,
+    "Classement publie" => $classements,
+    "Player publie" => $players,
+    "Classement NB votes" =>  $nb_classement_votes,
+  );
+
+  // $test = json_encode($results);
+
+  return $results;
 }
--- wp-content/themes/t-vkrz-3/function/API/routes.php
+++ wp-content/themes/t-vkrz-3/function/API/routes.php
@@ -1,21 +1,25 @@
 <?php
 
 
-add_action('rest_api_init', function(){
+add_action('rest_api_init', function () {
 
-    // List of contenders into a ranking
-    register_rest_route( 'vkrz/v1', '/ranking/(?P<id_ranking>[\d]+)', array(
-        'methods' => 'GET',
-        'callback' => 'get_single_ranking',
-        'args' => [
-            'id_ranking'
-        ]
-    ));
+  register_rest_route('vkrz/v1', '/info', array(
+    'methods' => 'GET',
+    'callback' => 'get_info'
+  ));
 
-    // Create contender
-    register_rest_route('vkrz/v1', '/addcontender/', array(
-        'methods' => 'GET',
-        'callback' => 'add_contender_from_api',
-    ));
+  // List of contenders into a ranking
+  register_rest_route('vkrz/v1', '/ranking/(?P<id_ranking>[\d]+)', array(
+    'methods' => 'GET',
+    'callback' => 'get_single_ranking',
+    'args' => [
+      'id_ranking'
+    ]
+  ));
+
+  // Create contender
+  register_rest_route('vkrz/v1', '/addcontender/', array(
+    'methods' => 'GET',
+    'callback' => 'add_contender_from_api',
+  ));
 });
-
