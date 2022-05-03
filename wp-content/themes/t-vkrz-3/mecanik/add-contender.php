e10d0eeef5625fbbdfb331882a2bfbf2f7ad6248
Romain Rullier <71820670+RomainRullier@users.noreply.github.com>
2022-05-03 13:43:01 +0200

    update

--- wp-content/themes/t-vkrz-3/mecanik/add-contender.php
+++ wp-content/themes/t-vkrz-3/mecanik/add-contender.php
@@ -41,59 +41,125 @@ if (false === ($data_t_created = get_transient('user_' . $user_id . '_get_creato
 <div class="app-content content recrutement-page">
   <div class="content-wrapper">
     <div class="content-body">
-      <div class="content-monitor apropos">
+      <div class="content-monitor">
         <div class="container">
           <div class="row mt-5">
             <div class="col-md-8 offset-md-2">
 
               <h1 class="text-uppercase text-center" id="h1-ajout-contender">
-                Ajouter un contender 🤟
+                <?php the_title(); ?> 🤟
               </h1>
 
-              <div class="gutenberg">
-                <?php the_content(); ?>
-              </div>
-
-              <div class="typeform mt-3">
-                <form method="POST" class="dropzone" id="form-ajout-contender" enctype="multipart/form-data">
-
-                    <div class="form-group">
-                      <input type="hidden" class="form-control p-2" id="idphoto" name="idphoto" placeholder="ID Photo" value="<?= uniqid() ?>">
+              <div class="row mt-3">
+                <div class="col-md-6">
+                  <?php 
+                    $posts = get_field('id_du_top_add_contender');
+                    if( $posts ): 
+                  ?>
+                    <div class="card animate__animated animate__flipInX card-developer-meetup">
+                      <div class="card-body rules-content">
+                        <div class="mb-1">
+                          <?php 
+                            foreach( $posts as $post ):
+                            setup_postdata( $post );
+                          ?>  
+                            <div class="title-win">
+                              <h4>
+                                <?php the_field('titre_de_la_sponso_t_sponso'); ?>
+                              </h4>
+                            </div>
+                          <?php 
+                            endforeach; 
+                            wp_reset_postdata();
+                          ?>
+                        </div>
+                        <div class="text-rules">
+                          <?php the_content(); ?>
+                        </div>
+                      </div>
+                      <?php 
+                        foreach( $posts as $post ):
+                        setup_postdata( $post );
+                      ?>
+                        <div class="card-footer share-content-sponso">
+                            <div class="text-left">
+                              <p>
+                                <?php the_field('top_propose_par_t_sponso'); ?>
+                              </p>
+                            </div>
+                            <div class="d-flex align-items-center reseaux-sponso m-0">
+                              <div class="logo-vkrz-sponso">
+                                <?php
+                                if (get_field('logo_de_la_sponso_t_sponso')) : ?>
+                                  <a href="<?php the_field('lien_de_la_sponso_t_sponso'); ?>" target="_blank">
+                                    <?php echo wp_get_attachment_image(get_field('logo_de_la_sponso_t_sponso', $id_top), 'large', '', array('class' => 'img-fluid')); ?>
+                                  </a>
+                                <?php endif; ?>
+                              </div>
+                              <div class="mt-2 social-media-sponso">
+                                <div class="d-flex buttons-social-media">
+                                  <?php if (have_rows('liste_des_liens_t_sponso')) : ?>
+                                    <?php while (have_rows('liste_des_liens_t_sponso')) : the_row(); ?>
+                                      <a href="<?php the_sub_field('lien_vers_t_sponso'); ?>" class="w-100 animate__jello animate__animated animate__delay-1s btn btn-max btn-outline-primary waves-effect waves-float waves-light" target="_blank">
+                                        <?php the_sub_field('intitule_t_sponso'); ?>
+                                      </a>
+                                    <?php endwhile; ?>
+                                  <?php endif; ?>
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+                          <div class="card-footer text-center p-20 m-0">
+                            <span class="t-rose">
+                              <?php the_field('fin_de_la_sponso_t_sponso'); ?>
+                            </span>
+                          </div>
+                        </div>
+                      <?php 
+                        endforeach; 
+                        wp_reset_postdata();
+                      ?>
                     </div>
+                  <?php endif; ?>
+                <div class="col-md-6">
+                  <div class="typeform">
+                    <form method="POST" class="dropzone" id="form-ajout-contender" enctype="multipart/form-data">
 
-                    <div class="form-group input-group-lg dropzone dropzone-area" id="dpz-single-file">
-                      <input type="file" class="form-control p-2" id="url_visual" name="url_visual" placeholder="URL Visual">
-                      <div class="dz-message text-center pb-5">Déposez le fichier ici ou cliquez pour le télécharger.</div>
-                    </div>
+                        <div class="form-group">
+                          <input type="hidden" class="form-control p-2" id="idphoto" name="idphoto" placeholder="ID Photo" value="<?= uniqid() ?>">
+                        </div>
 
-                    <div class="form-group input-group-lg">
-                      <input type="text" class="form-control p-2" id="pseudo" name="pseudo" placeholder="Pseudo">
-                    </div>
+                        <div class="form-group input-group-lg dropzone dropzone-area" id="dpz-single-file">
+                          <input type="file" class="form-control p-2" id="url_visual" name="url_visual" placeholder="URL Visual">
+                          <div class="dz-message text-center pb-3">Déposez le fichier ici ou cliquez pour le télécharger.</div>
+                        </div>
 
-                    <!-- <div class="form-group input-group-lg">
-                      <input type="number" class="form-control p-2" id="id_top" name="id_top" placeholder="ID Top">
-                    </div> -->
+                        <div class="form-group input-group-lg">
+                          <input type="text" class="form-control p-2" id="pseudo" name="pseudo" placeholder="Pseudo">
+                        </div>
 
-                    <div class="form-group input-group-lg">
-                      <select name="id_top" id="id_top" class="selectpicker" data-live-search="true" data-hide-disabled="true" data-width="100%" title="Sélectionnez ton Top">
-                        <?php
-                        foreach ($data_t_created['creator_tops'] as $item) : ?>
-                          <?php if (!get_field('private_t', $item['top_id'])) : ?>
-                            <option value="<?= $item['top_id']; ?>" data-tokens="<?= $item['top_title']; ?>"><?= $item['top_title']; ?></option>
+                        <!-- <div class="form-group input-group-lg">
+                          <input type="number" class="form-control p-2" id="id_top" name="id_top" placeholder="ID Top">
+                        </div> -->
+
+                        <div class="form-group input-group-lg">
+                          <?php
+                          if(get_field('id_du_top_add_contender')) : ?>
+                            <input type="hidden" class="form-control p-2" id="id_top" name="id_top" value="<?php the_field('id_du_top_add_contender'); ?>">
                           <?php endif; ?>
-                        <?php endforeach; ?>
-                      </select>
+                        </div>
+
+                        <div class="text-center mt-2">
+                          <button type="submit" name="envoyer" id="ajout-contender" class="btn btn-primary btn-lg">Envoyer</button>
+                        </div>
+
+                    </form>
+
+                    <div class="notification mt-2" style="display: none;">
+                      <div class="notification-text">
+                        <span style="font-size: 1.5rem;">Contender ajouté avec succès ! 🚀</span>
+                      </div>
                     </div>
-
-                    <div class="text-center mt-2">
-                      <button type="submit" name="envoyer" id="ajout-contender" class="btn btn-primary btn-lg">Envoyer</button>
-                    </div>
-
-                </form>
-
-                <div class="notification mt-2" style="display: none;">
-                  <div class="notification-text">
-                    <span style="font-size: 1.5rem;">Contender ajouté avec succès ! 🚀</span>
                   </div>
                 </div>
               </div>
