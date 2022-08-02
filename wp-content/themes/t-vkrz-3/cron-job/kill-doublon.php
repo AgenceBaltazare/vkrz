a4b7f25df73176e9a9e667ea255bd5dc9171cd08
Vergano <vergano.g@gmail.com>
2022-08-02 07:56:16 +0200

    Update kill-doublon.php

--- wp-content/themes/t-vkrz-3/cron-job/kill-doublon.php
+++ wp-content/themes/t-vkrz-3/cron-job/kill-doublon.php
@@ -8,14 +8,15 @@ $doublon = array();
 
 $vainkeur = new WP_Query(array(
     "post_type"              => "vainkeur",
-    "posts_per_page"         => -1,
+    "posts_per_page"         => 99999999,
     "fields"                 => "ids",
     "post_status"            => array("publish"),
     "orderby"                => "date",
     "order"                  => "ASC",
     "ignore_sticky_posts"    => true,
     "update_post_meta_cache" => false,
-    "no_found_rows"          => false
+    "no_found_rows"          => false,
+    'offset'                 => 17067
 ));
 while ($vainkeur->have_posts()) : $vainkeur->the_post();
 
