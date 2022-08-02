084bb1f555c20739e2103d2b3da3cc84c2113345
Vergano <vergano.g@gmail.com>
2022-08-02 09:33:16 +0200

    Update count.php

--- wp-content/themes/t-vkrz-3/cron-job/count.php
+++ wp-content/themes/t-vkrz-3/cron-job/count.php
@@ -39,27 +39,6 @@ $total_money = 0;
 $total_disponible = 0;
 $total_money_creator = 0;
 
-$vainkeur = new WP_Query(array(
-    "post_type"              => "vainkeur",
-    "posts_per_page"         => -1,
-    "fields"                 => "ids",
-    "post_status"            => "publish",
-    "orderby"                => "date",
-    "order"                  => "ASC",
-    "ignore_sticky_posts"    => true,
-    "update_post_meta_cache" => false,
-    "no_found_rows"          => false
-));
-while ($vainkeur->have_posts()) : $vainkeur->the_post();
-
-    $total_money = $total_money + get_field('money_vkrz');
-    $total_disponible = $total_disponible + get_field('money_vkrz');
-
-    if (get_field('money_creator_vkrz')) {
-        $total_money_creator = $total_money_creator + get_field('money_creator_vkrz');
-    }
-
-endwhile;
 
 $pr_triche      = $nb_tops_triche * 100 / $count_tops;
 $pr_complete    = $nb_tops_complete * 100 / $count_tops;
@@ -72,7 +51,5 @@ echo "Tops recommencés : " . $nb_tops_again . "\n";
 echo "Triche : " . $nb_tops_triche . " (" . round($pr_triche) . "%)\n";
 echo "Top 3 : " . $type_top_3 . " (" . round($pr_top3) . "%)\n";
 echo "Complet : " . $type_top_complet . " (". round($pr_complet)."%)\n";
-echo "Money vainkeur : " . $total_money . "\n";
-echo "Money creator : " . $total_money_creator . "\n";
-echo "Money disponible : " . $total_disponible . "\n";
+
 
