6ca8b9622897b3873ab2629e1fdcd8c436abbddf
Vergano <vergano.g@gmail.com>
2022-08-09 13:03:16 +0200

    Update check-toplist.php

--- wp-content/themes/t-vkrz-3/cron-job/check-toplist.php
+++ wp-content/themes/t-vkrz-3/cron-job/check-toplist.php
@@ -12,15 +12,16 @@ $classements = new WP_Query(array(
     'meta_query'             => array(
         array(
             'key' => 'id_vainkeur_r',
-            'compare' => 'NOT EXISTS'
+            'value' => '',
+            'compare' => '='
         )
     )
 ));
 if ($classements->have_posts()) {
     foreach ($classements->posts as $classement) {
 
-        wp_delete_post($classement, true);
+        //wp_delete_post($classement, true);
         echo "TopList " . $classement . "\n";
-        
+
     }
 }
