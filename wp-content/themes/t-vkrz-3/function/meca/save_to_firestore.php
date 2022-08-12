39cc43736f64d9a35c307cf5dcd6980a396a3d0b
Vergano <vergano.g@gmail.com>
2022-08-12 19:05:54 +0200

    Update save_to_firestore.php

--- wp-content/themes/t-vkrz-3/function/meca/save_to_firestore.php
+++ wp-content/themes/t-vkrz-3/function/meca/save_to_firestore.php
@@ -2,19 +2,19 @@
 function save_to_firestore($id_top, $current_id_vainkeur, $id_ranking){
     
     $id_resume = get_resume_id($id_top);
-    wp_update_post(array('ID' => $id_ranking));
-    wp_update_post(array('ID' => $current_id_vainkeur));
-    wp_update_post(array('ID' => $id_resume));
+    wp_update_post($id_ranking);
+    wp_update_post($current_id_vainkeur);
+    wp_update_post($id_resume);
 
-    update_field('maj_vkrz', 'iiiii', 469420);
+    update_field('maj_vkrz', 'fdfd', 469420);
 
 }
 
 function save_elo_to_firestore($contender1, $contender2){
 
-    wp_update_post(array('ID' => $contender1));
-    wp_update_post(array('ID' => $contender2));
+    wp_update_post($contender1);
+    wp_update_post($contender2);
 
-    update_field('maj_vkrz', 'elo', 469396);
+    update_field('maj_vkrz', 'wa', 469396);
     
 }
