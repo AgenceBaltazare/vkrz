d827ec274a75b32f9a7e3e8ee4ee0b4af71e1eb4
Vergano <vergano.g@gmail.com>
2022-08-16 08:41:44 +0200

    route pour toplistVAINKEUR

--- wp-content/themes/t-vkrz-3/partials/top-card.php
+++ wp-content/themes/t-vkrz-3/partials/top-card.php
@@ -8,23 +8,23 @@ if($list_cat){
     }
 }
 if($cat_id){
-    $icontop = get_field('icone_cat', 'term_' . $cat_id);
+    $cat_icon = get_field('icone_cat', 'term_' . $cat_id);
 }
 else{
-    $icontop = "<span class='va va-crossed-swords va-lg'></span>";
+    $cat_icon = "<span class='va va-crossed-swords va-lg'></span>";
 }
 ?>
 <a href="<?php the_permalink($id_top); ?>" class="top-card">
     <div class="d-flex align-items-center">
         <div class="avatar">
-            <?php $minia = get_the_post_thumbnail_url($id_top, 'large') ?>
+            <?php $minia = get_the_post_thumbnail_url($id_top, 'large'); ?>
             <span class="avatar-picture avatar-top" style="background-image: url(<?php echo $minia; ?>);"></span>
         </div>
         <div class="font-weight-bold topnamebestof">
             <div class="media-body">
                 <div class="media-heading">
                     <h6 class="cart-item-title mb-0">
-                        Top <?php the_field('count_contenders_t', $id_top); ?> <?php echo $icontop; ?> <?php echo get_the_title($id_top); ?>
+                        Top <?php the_field('count_contenders_t', $id_top); ?> <?php echo $cat_icon; ?> <?php echo get_the_title($id_top); ?>
                     </h6>
                     <span class="cart-item-by legende">
                         <?php the_field('question_t', $id_top); ?>
