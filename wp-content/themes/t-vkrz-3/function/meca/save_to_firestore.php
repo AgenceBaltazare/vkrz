09110e80e08982733532d8fc96ca1680942bbe0c
Vergano <vergano.g@gmail.com>
2022-08-17 00:49:56 +0200

    Update save_to_firestore.php

--- wp-content/themes/t-vkrz-3/function/meca/save_to_firestore.php
+++ wp-content/themes/t-vkrz-3/function/meca/save_to_firestore.php
@@ -10,25 +10,9 @@ function save_to_firestore($id_top, $id_vainkeur, $id_ranking){
 
 function save_elo_to_firestore($contender_1, $contender_2){
 
-    //wp_update_post(array('ID' => $contender_1));
-    //wp_update_post(array('ID' => $contender_2));
+    wp_update_post(array('ID' => $contender_1));
+    wp_update_post(array('ID' => $contender_2));
 
-    $post_id = '471001';
-    $data = new stdClass();
-
-    $data->custom_fields = new stdClass();
-    $data->custom_fields->ELO_c = 'test';
-
-    error_log(print_r($data));
-
-    // { 
-    //     custom_fields: {
-    //        ELO_c: 'test'
-    //     }
-    //  }
-    
-
-    apply_filters('firebase_save_data_to_database', 'firestore', 'wpContender', $post_id, $data);
 }
 
 function save_elo_to_firestore_delete($id_ranking, $id_vainkeur){
