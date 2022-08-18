bc700c17e826de58c5a5f47b1b3dd75633a580ef
EL MOBACHI Adil <adilelmoba20@gmail.com>
2022-08-18 14:24:07 +0200

    maj

--- wp-content/themes/t-vkrz-3/user/account.php
+++ wp-content/themes/t-vkrz-3/user/account.php
@@ -309,6 +309,13 @@ if ($list_user_toplists) {
                       <div class="col-12">
                         <div class="card text-center loader-list card-voile" data-idtop="<?php echo $id_top; ?>" data-topurl="<?php echo get_permalink($id_top) ?>">
                           <div class="voile-gif" style="background-image: url(<?php bloginfo('template_directory'); ?>/assets/images/gif/wait-<?php echo rand(1, 7); ?>.gif)"></div>
+                          <a href="https://elmobachiadil.com" target="_blank" class="participate-gif-top">
+                            <span data-toggle="tooltip" data-placement="top" title="Participate to the best Waiting GIF Top" data-original-title="Participate to the best Waiting GIF Top">
+                              <span class="badge badge-pill badge-light-primary">
+                                <span class="ico text-center va va-trophy va-lg"></span>
+                              </span>
+                            </span>
+                          </a>
                           <div class="card-body">
                             <div class="content-card">
                               <div class="loader-block">
