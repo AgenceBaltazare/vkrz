2715acd762ea8d6972577bc023ee75a3b6bdaac6
Vergano <vergano.g@gmail.com>
2022-08-23 15:04:54 +0200

    Update check-toplist.php

--- wp-content/themes/t-vkrz-6/cron-job/check-toplist.php
+++ wp-content/themes/t-vkrz-6/cron-job/check-toplist.php
@@ -3,23 +3,19 @@ include __DIR__ . '/../../../../wp-load.php';
 
 $classements = new WP_Query(array(
     'post_type'              => 'classement',
-    'posts_per_page'         => -1,
+    'posts_per_page'         => 200,
     'fields'                 => 'ids',
     'post_status'            => 'publish',
     'ignore_sticky_posts'    => true,
     'update_post_meta_cache' => false,
     'no_found_rows'          => false,
-    'date_query' => array(
-        array(
-            'column' => 'post_date_gmt',
-            'before' => '1 day ago',
-        ),
-    ),
+    'orderby'                => 'date',
+    'order'                  => 'DESC',
 ));
 if ($classements->have_posts()) {
     $i=1; foreach ($classements->posts as $classement) {
 
-        echo $i . " -> TopList " . $classement . "\n";
+        echo $i . " -> TopList " . get_the_title($classement) . "\n";
 
         $i++;
     }
