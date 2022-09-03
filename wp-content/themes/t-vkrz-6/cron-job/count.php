eeb724526bff2e12523c3b0a7a92b06941cdf818
Vergano <vergano.g@gmail.com>
2022-09-03 08:19:19 +0200

    Create count.php

---
+++ wp-content/themes/t-vkrz-6/cron-job/count.php
@@ -0,0 +1,38 @@
+<?php
+include __DIR__ . '/../../../../wp-load.php';
+
+$classement = new WP_Query(
+    array(
+        'ignore_sticky_posts'    => true,
+        'update_post_meta_cache' => false,
+        'no_found_rows'          => true,
+        'post_type'              => 'classement',
+        'orderby'                => 'date',
+        'order'                  => 'DESC',
+        'posts_per_page'         => -1,
+        'date_query'     => array(
+            'after' => array(
+                'year'  => 2022,
+                'month' => 8,
+                'day'   => 19,
+            )
+        )
+    )
+);
+$nb_votes   = 0;
+$nb_tops    = 0;
+while ($classement->have_posts()) : $classement->the_post();
+
+    $id_ranking   = get_the_ID();
+    $nb_vote      = get_field('nb_votes_r', $id_ranking);
+
+    $nb_votes     = $nb_votes + $nb_vote;
+
+    echo $id_ranking . " - Votes : " . $nb_vote . "\n";
+
+    $nb_tops++;
+
+endwhile;
+wp_reset_query();
+
+echo "\n\n Résultat : " . $nb_tops . " " . $nb_votes;
