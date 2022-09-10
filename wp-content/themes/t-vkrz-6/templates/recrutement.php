a6034771f3dc54b620e2c081b08e71428d3109ab
Vergano <vergano.g@gmail.com>
2022-09-10 16:28:52 +0200

    Update recrutement.php

--- wp-content/themes/t-vkrz-6/templates/recrutement.php
+++ wp-content/themes/t-vkrz-6/templates/recrutement.php
@@ -8,10 +8,14 @@
                 <div class="container">
                     <div class="row mt-5">
                         <div class="col-md-8 offset-md-2">
-                            <h1 class="text-uppercase"><span class="va va-raised-fist va-lg"></span> Tu souhaites <span class="t-rose">créer des Tops</span> ?</h1>
-                            <h3 class="mt-1">La 1<sup>ère</sup> étape pour rejoindre l'ékipe de créateurs <span class="va va-man-singer va-1x"></span> est de répondre à ceci <span class="va va-backhand-index-pointing-down va-1x"></span></h3>
+                            <h1 class="text-uppercase">
+                                <?php the_field('phrase_recruement_#1'); ?>
+                            </h1>
+                            <h3 class="mt-1">
+                                <?php the_field('phrase_recruement_#2'); ?>
+                            </h3>
                             <div class="typeform mt-3">
-                                <iframe src="https://tally.so/embed/npG0qw" width="100%" height="100%" frameborder="0" marginheight="0" marginwidth="0" title="Recrutement"></iframe>
+                                <iframe src="<?php the_field('formulaire_recrutement'); ?>" width="100%" height="100%" frameborder="0" marginheight="0" marginwidth="0" title="Recrutement"></iframe>
                             </div>
                         </div>
                     </div>
