acc3ce8fad7c21e809019aa35d439de358e5425e
EL MOBACHI Adil <adilelmoba20@gmail.com>
2022-11-26 17:13:51 +0100

    discord

--- wp-content/themes/t-vkrz-6/function/meca/to_discord.php
+++ wp-content/themes/t-vkrz-6/function/meca/to_discord.php
@@ -3,5 +3,5 @@
 function to_discord($typeMessage, $data) {
   $data = json_encode($data);
   
-  shell_exec("/usr/local/bin/node /var/www/vainkeurz.com/index.js 2>&1 '$typeMessage' '$data'");
+  shell_exec("/usr/bin/node /var/www/vainkeurz.com/index.js 2>&1 '$typeMessage' '$data'");
 }
