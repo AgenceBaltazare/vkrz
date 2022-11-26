2f47c2fc77a35302631c2be8cc018d7cc5411683
EL MOBACHI Adil <adilelmoba20@gmail.com>
2022-11-26 17:20:42 +0100

    discord

--- wp-content/themes/t-vkrz-6/function/meca/to_discord.php
+++ wp-content/themes/t-vkrz-6/function/meca/to_discord.php
@@ -3,5 +3,6 @@
 function to_discord($typeMessage, $data) {
   $data = json_encode($data);
   
-  shell_exec("/usr/bin/node /var/www/vainkeurz.com/index.js 2>&1 '$typeMessage' '$data'");
+  shell_exec("/usr/bin/node /var/www/vainkeurz.com/web/index.js 2>&1 '$typeMessage' '$data'"); // PROD
+  // shell_exec("/usr/local/bin/node /Applications/MAMP/htdocs/vkrz/index.js 2>&1 '$typeMessage' '$data'"); // LOCAL 
 }
