a71ab072015c1ee5aceae017ceadcf39681cb85e
adilelmoba <adilelmoba20@gmail.com>
2023-02-07 17:50:58 +0100

    sync users firebase

--- wp-content/themes/t-vkrz-6/function/webhooks/new_vainkeur.php
+++ wp-content/themes/t-vkrz-6/function/webhooks/new_vainkeur.php
@@ -104,6 +104,26 @@ function new_vainkeur($user_id){
             if($_GET['codeinvit']) {
                 deal_referral($_GET['codeinvit'], $id_vainkeur, 200);
             }
+
+            // SEND/UPDATE USER TO FIREBASE
+            $user_infos  = get_user_infos($uuid_vainkeur);
+
+            $utilisateur                   = new stdClass();
+            $utilisateur->Pseudo           = $user_infos['pseudo'];
+            $utilisateur->Image            = $user_infos['avatar'];
+            $utilisateur->Email            = $user_infos['user_email'];
+            $utilisateur->UUID             = $user_infos['uuid_vainkeur'];
+            $utilisateur->idVainkeur       = $user_infos['id_vainkeur'];
+            $utilisateur->level            = $user_infos['level_number'];
+            $utilisateur->role             = $user_infos['user_role'];
+            $utilisateur->Twitch           = get_userdata($user_id)->twitch_user;
+            $utilisateur->YouTube          = get_userdata($user_id)->youtube_user;
+            $utilisateur->Instagram        = get_userdata($user_id)->Instagram_user;
+            $utilisateur->TikTok           = get_userdata($user_id)->tiktok_user;
+            $utilisateur->Twitter          = get_userdata($user_id)->twitter_user;
+            $utilisateur->RegistrationDate = date("d-m-Y H:i:s", strtotime(get_userdata( $user_id )->user_registered));
+            
+            apply_filters('firebase_save_data_to_database', "firestore", "utilizateurs", get_userdata($user_id)->user_login, $utilisateur);
         }
     }
 
@@ -129,3 +149,21 @@ function new_vainkeur($user_id){
     <?php
     echo ob_get_clean();
 }
+
+// SEND/UPDATE USER TO FIREBASE
+add_action( 'set_user_role', function( $user_id ) 
+{
+    
+    add_action( 'profile_update', function( $user_id )
+    {
+        $uuid_vainkeur = get_field('uuiduser_user', 'user_' . $user_id);
+
+        $user_infos    = get_user_infos($uuid_vainkeur);
+
+        $utilisateur                   = new stdClass();
+        $utilisateur->role             = $user_infos['user_role'];
+
+        apply_filters('firebase_save_data_to_database', "firestore", "utilizateurs", get_userdata($user_id)->user_login, $utilisateur);
+    } );
+
+} );
